897eed89fbfa2511f7e18d4a68bd51062ec8dbbf
Mark Stiles <stiles.mark@gmail.com>
2019-07-08 15:24:14 -0400

    * same as last

--- code/Areas/SitecoreCognitiveServices/Controllers/BlogCastController.cs
+++ code/Areas/SitecoreCognitiveServices/Controllers/BlogCastController.cs
@@ -51,15 +51,17 @@ namespace SitecoreCognitiveServices.Feature.BlogCast.Areas.SitecoreCognitiveServ
 
             var cleanText = Regex.Replace(sb.ToString(), "<.*?>", " ").Replace("\"", "").Replace("&", " and ").Replace("   ", " ").Replace("  ", " ");
 
-            var relativePath = $"temp\\blogcast-{page.ID.Guid.ToString("N")}.mp3";
-            var filePath = $"{Request.PhysicalApplicationPath}{relativePath}";
+            var maleRelativePath = $"temp\\blogcast-{page.ID.Guid.ToString("N")}-male.mp3";
+            var maleFilePath = $"{Request.PhysicalApplicationPath}{maleRelativePath}";
+            var femaleRelativePath = $"temp\\blogcast-{page.ID.Guid.ToString("N")}-female.mp3";
+            var femaleFilePath = $"{Request.PhysicalApplicationPath}{femaleRelativePath}";
 
             var locale = SpeechLocaleOptions.enUS;
-            var voice = VoiceName.EnUsGuyNeural;
             var gender = GenderOptions.Male;
             var audioFormat = AudioOutputFormatOptions.Audio24Khz160KBitRateMonoMp3;
 
-            SpeechService.TextToFile(cleanText, filePath, locale, voice, gender, audioFormat);
+            SpeechService.TextToFile(cleanText, maleFilePath, locale, VoiceName.EnUsGuyNeural, gender, audioFormat);
+            SpeechService.TextToFile(cleanText, femaleFilePath, locale, VoiceName.EnUsJessaNeural, gender, audioFormat);
 
             return Json(new { Succeeded = true });
         }
@@ -67,11 +69,15 @@ namespace SitecoreCognitiveServices.Feature.BlogCast.Areas.SitecoreCognitiveServ
         public ActionResult RemoveMediaFile(string id, string db, string language)
         {
             Guid g = Guid.Parse(id);          
-            var relativePath = $"temp\\blogcast-{g.ToString("N")}.mp3";
-            var filePath = $"{Request.PhysicalApplicationPath}{relativePath}";
+            var maleRelativePath = $"temp\\blogcast-{g.ToString("N")}-male.mp3";
+            var maleFilePath = $"{Request.PhysicalApplicationPath}{maleRelativePath}";
+            var femaleRelativePath = $"temp\\blogcast-{g.ToString("N")}-female.mp3";
+            var femaleFilePath = $"{Request.PhysicalApplicationPath}{femaleRelativePath}";
 
-            if (System.IO.File.Exists(filePath))
-                System.IO.File.Delete(filePath);
+            if (System.IO.File.Exists(maleFilePath))
+                System.IO.File.Delete(maleFilePath);
+            if (System.IO.File.Exists(femaleFilePath))
+                System.IO.File.Delete(femaleFilePath);
 
             return Json(new { Succeeded = true });
         }
